cfa01d1b21d25f0c463147deb920bcbc9042e675
sarojini02 <149888313+sarojini02@users.noreply.github.com>
2023-12-05 20:54:09 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,89 @@
+// Questions that will be asked
+const Questions = [{
+	q: "What is the Index helps to understand the growth of various sectors of the Economy?",
+	a: [{ text: "IIP", isCorrect: true },
+	{ text: "CPI", isCorrect: false },
+	{ text: "WPI", isCorrect: false },
+	{ text: "None of the above", isCorrect: false }
+	]
+
+},
+{
+	q: "What is the capital of Thailand?",
+	a: [{ text: "Lampang", isCorrect: false, isSelected: false },
+	{ text: "Phuket", isCorrect: false },
+	{ text: "Ayutthaya", isCorrect: false },
+	{ text: "Bangkok", isCorrect: true }
+	]
+
+},
+{
+	q: "Which Indian cricketer has captained the Indian team in the most World Cup matches?",
+	a: [{ text: "Sourav Ganguly", isCorrect: false },
+	{ text: "Virat Kohli", isCorrect: false },
+	{ text: "MS Dhoni", isCorrect: true },
+	{ text: "Rohit Sharma", isCorrect: false }
+	]
+
+}
+
+]
+
+let currQuestion = 0
+let score = 0
+
+function loadQues() {
+	const question = document.getElementById("ques")
+	const opt = document.getElementById("opt")
+
+	question.textContent = Questions[currQuestion].q;
+	opt.innerHTML = ""
+
+	for (let i = 0; i < Questions[currQuestion].a.length; i++) {
+		const choicesdiv = document.createElement("div");
+		const choice = document.createElement("input");
+		const choiceLabel = document.createElement("label");
+
+		choice.type = "radio";
+		choice.name = "answer";
+		choice.value = i;
+
+		choiceLabel.textContent = Questions[currQuestion].a[i].text;
+
+		choicesdiv.appendChild(choice);
+		choicesdiv.appendChild(choiceLabel);
+		opt.appendChild(choicesdiv);
+	}
+}
+
+loadQues();
+
+function loadScore() {
+	const totalScore = document.getElementById("score")
+	totalScore.textContent = `You scored ${score} out of ${Questions.length}`
+}
+
+
+function nextQuestion() {
+	if (currQuestion < Questions.length - 1) {
+		currQuestion++;
+		loadQues();
+	} else {
+		document.getElementById("opt").remove()
+		document.getElementById("ques").remove()
+		document.getElementById("btn").remove()
+		loadScore();
+	}
+}
+
+function checkAns() {
+	const selectedAns = parseInt(document.querySelector('input[name="answer"]:checked').value);
+
+	if (Questions[currQuestion].a[selectedAns].isCorrect) {
+		score++;
+		console.log("Correct")
+		nextQuestion();
+	} else {
+		nextQuestion();
+	}
+}
